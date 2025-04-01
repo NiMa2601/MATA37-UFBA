6857795409a358b1a20587a81278cd6d820bb411
NiMa2601 <magris@ufba.br>
2025-04-01 16:31:07 -0300

    EXERCICIO 1

--- LISTA01.py
+++ LISTA01.py
@@ -12,3 +12,14 @@ calcule = (2*4 == 0)
 print (calcule)
 calcule = 15%4 < 19//6
 print (calcule)
+
+print ("oi,")
+print ("pessoal")
+
+print ("Oi,", end="")
+print ("pessoal de novo!")
+
+print ("informe seu nome")
+nome = input()
+print ("oi", nome)
+
