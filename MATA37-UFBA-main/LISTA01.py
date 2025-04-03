6d224970af05c47453f867992f95fc6f874ba66b
NiMa2601 <magris@ufba.br>
2025-04-03 16:28:56 -0300

    Add files via upload

---
+++ MATA37-UFBA-main/LISTA01.py
@@ -0,0 +1,30 @@
+Nome = ('luis')  
+print (Nome)  
+NotaAluno = ('X')  
+print (NotaAluno)   
+NomeAluno = ('Luis')
+print (NomeAluno)
+MatAluno = ('224123')
+print (MatAluno)
+sexo = ('F')
+print (sexo)
+calcule = (2*4 == 0)
+print (calcule)
+calcule = 15%4 < 19//6
+print (calcule)
+
+print ("oi,")
+print ("pessoal")
+
+print ("Oi,", end="")
+print ("pessoal de novo!")
+
+print ("informe seu nome")
+nome = input()
+print ("oi", nome)
+
+
+Nome = "Pat"
+Idade = "54"
+Cidade = "Salvador"
+print (Nome, Idade, Cidade, sep="-")
---
+++ MATA37-UFBA-main/LISTA02.py
@@ -0,0 +1,16 @@
+Nome = "Pat"
+Idade = "54"
+Cidade = "Salvador"
+print (Nome, Idade, Cidade, sep="-")
+
+
+Cliente = "João"
+Total_da_Compra = "54,90"
+print ("Cliente:", Cliente, "Total da Compra:", Total_da_Compra, sep="\n")
+
+
+Nome = imput("Nome") 
+
+
+
+
